cd646aa2419f85c71b50d82b9060b791509ff2bd
NikBar01 <137397752+NikBar01@users.noreply.github.com>
2025-01-26 18:41:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,12 @@ def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.content
 
-image_bytes = query({
-	"inputs": "Astronaut riding a horse",
-})
-
-# You can access the image with PIL.Image for example
-
-image = Image.open(io.BytesIO(image_bytes))
-image.show()
+def res (text)
+	image_bytes = query({
+	"inputs": text,
+	})
+	image = Image.open(io.BytesIO(image_bytes))
+	image.show()
 
 st.title('Новая улучшенная классификации изображений в облаке Streamlit')
 title = st.text_input("Movie title", "Life of Brian")
