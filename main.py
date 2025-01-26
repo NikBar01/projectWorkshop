0a680141a600ce551a250d44f30ca80dd423e6a1
NikBar01 <137397752+NikBar01@users.noreply.github.com>
2025-01-26 18:25:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import streamlit as st
 
 API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-3.5-large"
 headers = {"Authorization": "Bearer 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"}
@@ -16,3 +17,13 @@ import io
 from PIL import Image
 image = Image.open(io.BytesIO(image_bytes))
 image.show()
+
+st.title('Новая улучшенная классификации изображений в облаке Streamlit')
+title = st.text_input("Movie title", "Life of Brian")
+img = load_image()
+result = st.button('Распознать изображение')
+if result:
+    x = preprocess_image(img)
+    preds = model.predict(x)
+    st.write('**Результаты распознавания:**')
+    print_predictions(preds)
