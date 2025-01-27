6e8d49824549339163039e70c8f44abe911e4d74
NikBar01 <137397752+NikBar01@users.noreply.github.com>
2025-01-27 12:32:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,13 @@ from transformers import T5ForConditionalGeneration, T5Tokenizer
 API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-3.5-large"
 headers = {"Authorization": "Bearer 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"}
 
+API_URL_TR = "https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-ru-en"
+headersTR = {"Authorization": "Bearer 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"}
 
 
-# API_URL_TR = "https://api-inference.huggingface.co/models/facebook/mbart-large-50-many-to-many-mmt"
-# headers_TR = {"Authorization": "Bearer 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"}
-
-# def query_TR(payload):
-# 	response = requests.post(API_URL_TR, headers=headers_TR, json=payload)
-# 	return response.json()
+def queryTR(payload):
+    response = requests.post(API_URL_TR, headers=headersTR, json=payload)
+    return response.json()
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
@@ -30,26 +29,15 @@ def res (text):
 	image = Image.open(io.BytesIO(image_bytes))
 	return image
 
-# model_name = 'jbochi/madlad400-3b-mt'
-# model = T5ForConditionalGeneration.from_pretrained(model_name, device_map="auto")
-# tokenizer = T5Tokenizer.from_pretrained(model_name)
-
-# text = "<2pt> I love pizza!"
-# input_ids = tokenizer(text, return_tensors="pt").input_ids.to(model.device)
-# outputs = model.generate(input_ids=input_ids)
-
-# translet = tokenizer.decode(outputs[0], skip_special_tokens=True)
-
 st.title('Преобразование текста в изображение')
 title = st.text_input("Описание", "")
-
+output = queryTR({
+    "inputs": title,
+})
 result = st.button('Получить изображение')
 if result:
-	img = res(title)
+	img = res(output[0]['translation_text'])
 	st.image(img, caption="")
-
-
-
 
 device = 'cuda' #or 'cpu' for translate on cpu
 
