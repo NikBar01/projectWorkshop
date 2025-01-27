574d734fd7c0be0aa47f9e2e8898867ab5c900ff
NikBar01 <137397752+NikBar01@users.noreply.github.com>
2025-01-27 11:59:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -46,3 +46,24 @@ result = st.button('Получить изображение')
 if result:
 	img = res(title)
 	st.image(img, caption="")
+
+
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+
+device = 'cuda' #or 'cpu' for translate on cpu
+
+model_name = 'utrobinmv/t5_translate_en_ru_zh_small_1024'
+model = T5ForConditionalGeneration.from_pretrained(model_name)
+model.to(device)
+tokenizer = T5Tokenizer.from_pretrained(model_name)
+
+prefix = 'translate to zh: '
+src_text = prefix + "Цель разработки — предоставить пользователям личного синхронного переводчика."
+
+# translate Russian to Chinese
+input_ids = tokenizer(src_text, return_tensors="pt")
+
+generated_tokens = model.generate(**input_ids.to(device))
+
+result = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+print(result)
